dfa4e9f414c3ae710f28bf4d741e7f9e62407fe0
Ross Hudson <hello@rosshudson.co.uk>
2020-06-12 19:51:40 +0100

    Added thumbnail creation and clear download links

--- assets/main.js
+++ assets/main.js
@@ -22,6 +22,34 @@ function getWatermarkFile(width, height) {
 	return url;
 }
 
+function makeThumbnail(imageName, image, width, height) {
+	var canvas = document.createElement('canvas');
+	var ctx = canvas.getContext('2d');
+	var scaleFactor;
+	var fixedWidth = 500;
+
+	if(fixedWidth > width) {
+		scaleFactor = 1;
+		fixedWidth = width;
+	} else {
+		scaleFactor = 500 / width;
+	}
+
+	canvas.width = 500; // Set width to 500px;
+	canvas.height = height * scaleFactor;
+	ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+	document.getElementById("thumbnail-image").appendChild(canvas);
+
+
+	var downloadLink = document.createElement("a");
+	downloadLink.href = canvas.toDataURL();
+	downloadLink.download = imageName + "_thumbnail.jpeg";
+	downloadLink.textContent = "Download Thumbnail";
+	var paragraphWrapper = document.createElement("p");
+	paragraphWrapper.appendChild(downloadLink);
+	document.getElementById("thumbnail-image").appendChild(paragraphWrapper);
+}
+
 function processImage(selectedFile) {
 	var options = {
 		init(img) {
@@ -35,10 +63,21 @@ function processImage(selectedFile) {
 	image.onload = function() {
 		var width = image.naturalWidth;
                 var height = image.naturalHeight;
+		var imageName = selectedFile.name.split(".")[0] // Get filename without file extension.
+		makeThumbnail(imageName, image, width, height);
 		watermarkFile = getWatermarkFile(width, height);
-		console.log(watermarkFile);
 		watermark([selectedFile, watermarkFile], options)
-			.image(watermark.image.center(0.25))
-			.then(img => document.getElementById('result').appendChild(img))
+			.image(watermark.image.center(0.1))
+			.then(img => document.getElementById('result').appendChild(img)) // To-do: get as canvas
+
+		var downloadLink = document.createElement("a");
+		downloadLink.href = document.querySelector("#watermarked-image > img").src;
+		downloadLink.download = imageName + "_watermarked.jpeg";
+		downloadLink.textContent = "Download watermarked image";
+		var paragraphWrapper = document.createElement("p");
+		paragraphWrapper.appendChild(downloadLink);
+		document.getElementById("watermarked-image").appendChild(paragraphWrapper);
+
 	}
 }
+
