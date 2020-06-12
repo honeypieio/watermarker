1aafb1a428689b10860f57482d4ddbbe83107920
Ross Hudson <hello@rosshudson.co.uk>
2020-06-12 19:13:03 +0100

    Added different sizes of watermark

--- assets/main.js
+++ assets/main.js
@@ -7,13 +7,38 @@ window.onload = function() {
 	});
 }
 
+function getWatermarkFile(width, height) {
+	var url = baseUrl + '/assets/watermark-';
+
+	if(width > 1000) {
+		url += "1000px";
+	} else if (width > 750) {
+		url += "750px";
+	} else {
+		url += "500px";
+	}
+
+	url += ".png";
+	return url;
+}
+
 function processImage(selectedFile) {
 	var options = {
 		init(img) {
     			img.crossOrigin = 'anonymous'
   		}
 	}
-	watermark([selectedFile, baseUrl + '/assets/watermark.png'], options)
-	  .image(watermark.image.center(0.5))
-	  .then(img => document.getElementById('result').appendChild(img));
+
+	var image = new Image();
+
+	image.src = window.URL.createObjectURL(selectedFile);
+	image.onload = function() {
+		var width = image.naturalWidth;
+                var height = image.naturalHeight;
+		watermarkFile = getWatermarkFile(width, height);
+		console.log(watermarkFile);
+		watermark([selectedFile, watermarkFile], options)
+			.image(watermark.image.center(0.25))
+			.then(img => document.getElementById('result').appendChild(img))
+	}
 }
