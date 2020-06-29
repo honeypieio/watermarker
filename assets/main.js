f0f94fa5ce5171cce87d87d27795b78a5aeed3e0
Ross Hudson <hello@rosshudson.co.uk>
2020-06-29 13:06:01 +0100

    Added ability to scale both thumbnail and watermarked, improved file size efficiency.

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,39 @@
 var baseUrl = window.location;
 var zip;
+var thumbnailFixedWidth, watermarkedFixedWidth;
+var watermarkWidth;
 
 window.onload = function() {
 
 	document.getElementById("process-image-button").addEventListener("click", function(){
-		document.getElementById("output-sample-header").style.display = "block";
-		var selectedFiles = document.getElementById('image-to-process').files;
-		zip = new JSZip();
-		var totalFiles = Object.keys(selectedFiles).length;
-		var index = 0;
-		if(totalFiles > 0) {
+		
+		try {
+
+			thumbnailFixedWidth = document.getElementById("thumb-width").value;
+			watermarkedFixedWidth = document.getElementById("watermarked-width").value;
+			
+			if(isNaN(thumbnailFixedWidth)) {
+				throw "Please enter a valid value for thumnail width";
+			}
+
+			if(isNaN(watermarkedFixedWidth)) {
+				throw "Please enter a valid value for watermarked width";
+			}
+
+			var selectedFiles = document.getElementById('image-to-process').files;
+			var totalFiles = Object.keys(selectedFiles).length;
+
+			if(totalFiles == 0) {
+				throw "Please select at least one image file";
+			}
+
+			document.getElementById("output-sample-header").style.display = "block";
+			zip = new JSZip();
+			var index = 0;
 			processImage(selectedFiles, index, totalFiles);
+			
+		} catch(error) {
+			alert(error);
 		}
 	});
 }
@@ -22,47 +45,95 @@ function zipItUp() {
 	});
 }
 
-function getWatermarkFile(width, height) {
-	var url = baseUrl + '/assets/watermark-';
+function getWatermarkFile(width) {
+	var data;
 
 	if(width > 1000) {
-		url += "1000px";
+		watermarkWidth = "1000";
 	} else if (width > 750) {
-		url += "750px";
+		watermarkWidth = "750";
 	} else {
-		url += "500px";
+		watermarkWidth = "500";
 	}
 
-	url += ".png";
-	return url;
+	data = Watermarks[watermarkWidth];
+
+	return data;
 }
 
-function makeThumbnail(dir, imageName, image, width, height, callback) {
+function formatImage(dir, imageName, imageType, image, width, height, callback) {
 	var canvas = document.createElement('canvas');
 	var ctx = canvas.getContext('2d');
 	var scaleFactor;
-	var fixedWidth = 500;
+	var fixedWidth;
 
-	if(fixedWidth > width) {
-		scaleFactor = 1;
-		fixedWidth = width;
-	} else {
-		scaleFactor = 500 / width;
-	}
+	imageType == "thumbnail" ? fixedWidth = thumbnailFixedWidth: fixedWidth = watermarkedFixedWidth;
 
-	canvas.width = 500; // Set width to 500px;
+	scaleFactor = fixedWidth / width;
+
+	canvas.width = fixedWidth;
 	canvas.height = height * scaleFactor;
 	ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-	document.getElementById("thumbnail-image").innerHTML = "";
-	document.getElementById("thumbnail-image").appendChild(canvas);
+	
+	if(imageType == "watermarked"){
 
-	imageName += "-thumbnail.jpg";
-	dir.file(imageName, canvas.toDataURL("image/jpeg").split("data:image/jpeg;base64,")[1], { base64: true });
-	document.getElementById("thumbnail-image").appendChild(createDownloadLink(canvas.toDataURL("image/jpeg"), imageName, canvas.width, canvas.height));
-	setTimeout(function() {
+		var options = {
+			init(img) {
+				img.crossOrigin = 'anonymous'
+			}
+		}
+
+		watermarkFile = getWatermarkFile(watermarkedFixedWidth);
+		watermark([canvas.toDataURL("image/jpeg"), watermarkFile], options)
+		.blob(function(image, watermark) {
+			var watermarkedContext = image.getContext("2d");
+			watermarkedContext.save();
+			watermarkedContext.globalAlpha = 0.075;
+			var watermarkX = (canvas.width / 2) - (watermarkWidth / 2);
+			var watermarkY = (canvas.height /2) - (watermarkWidth / 2);	
+
+			watermarkedContext.drawImage(watermark, watermarkX, watermarkY);
+			watermarkedContext.restore();
+			var watermarkedDataUrl = watermarkedContext.canvas.toDataURL("image/jpeg");
+			setImage(
+				watermarkedDataUrl,
+				createDownloadLink(watermarkedDataUrl, imageName, canvas.width, canvas.height),
+				dir,
+				imageName,
+				imageType
+			);
+		
+		});
+
+	} else {
+
+		imageName += "-thumbnail.jpg";
+		setImage(
+			canvas.toDataURL("image/jpeg"), 
+			createDownloadLink(canvas.toDataURL("image/jpeg"), imageName, canvas.width, canvas.height),
+			dir,
+			imageName, 
+			imageType
+		);
+		
+	}
+
+	setTimeout(function(){
 		callback();
-	}, 500);
+	}, 250)
+
 }
+
+function setImage(data, downloadLink, dir, imageName, imageType) {
+	dir.file(imageName, data.split("data:image/jpeg;base64,")[1], { base64: true });
+	
+	document.getElementById(imageType + "-image").innerHTML = "";
+	var image = document.createElement("img");
+	image.src = data;
+	document.getElementById(imageType + "-image").appendChild(image);
+	document.getElementById(imageType + "-image").appendChild(downloadLink);
+}
+
 
 function createDownloadLink(src, imageName, width, height) {
 
@@ -80,12 +151,6 @@ function processImage(selectedFiles, index, totalFiles) {
 	document.getElementById("watermarked-image").innerHTML = "";
 	document.getElementById("thumbnail-image").innerHTML = "";
 
-	var options = {
-		init(img) {
-    			img.crossOrigin = 'anonymous'
-  		}
-	}
-
 	var selectedFile = selectedFiles[index];
 	var image = new Image();
 	image.src = window.URL.createObjectURL(selectedFile);
@@ -94,26 +159,16 @@ function processImage(selectedFiles, index, totalFiles) {
                 var height = image.naturalHeight;
 		var imageName = selectedFile.name.split(".")[0] // Get filename without file extension.
 		var dir = zip.folder(imageName);
-		makeThumbnail(dir, imageName, image, width, height, function(){
-
-			watermarkFile = getWatermarkFile(width, height);
-			watermark([selectedFile, watermarkFile], options)
-			.image(watermark.image.center(0.075))
-			.then(function(img) {
-				document.getElementById("watermarked-image").innerHTML = "";
-				document.getElementById('watermarked-image').appendChild(img);
-				imageName += "-watermarked.png";
-				setTimeout(function() {
-					dir.file(imageName, document.querySelector("#watermarked-image > img").src.split("data:image/png;base64,")[1], { base64: true });
-					document.getElementById("watermarked-image").appendChild(createDownloadLink(document.querySelector("#watermarked-image > img").src, imageName, width, height));
-					if(index == (totalFiles - 1)){
-						zipItUp();
-					} else {
-						index += 1;
-						processImage(selectedFiles, index, totalFiles);
-					}
-				}, 500);
-			})
+		formatImage(dir, imageName, "thumbnail", image, width, height, function(){
+			formatImage(dir, imageName, "watermarked", image, width, height, function() {
+			
+				if(index == (totalFiles - 1)){
+					zipItUp();
+				} else {
+					index += 1;
+					processImage(selectedFiles, index, totalFiles);
+				}
+			});
 		});
 	}
 }
